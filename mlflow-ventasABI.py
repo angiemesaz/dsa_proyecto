13a714f28972ebb2f26781215dcfcab18f43a2a5
davstvp2 <[200~ds.pena11@uniandes.edu.co>
2025-07-13 23:00:26 -0500

    ABT para creación de Dashboard

--- mlflow-ventasABI.py
+++ mlflow-ventasABI.py
@@ -1,4 +1,5 @@
 # Importar librerías
+pip install sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_diabetes
 from sklearn.ensemble import RandomForestRegressor
